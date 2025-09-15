c112a3e56530fae78afc56507c5721a2421b86a1
MK-1512 <mktech1512@gmail.com>
2025-09-15 22:44:10 +0530

    feat: Add WhiteNoise for static file serving

--- backend/chat_project/settings.py
+++ backend/chat_project/settings.py
@@ -33,6 +33,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -89,6 +90,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # <-- Ensure this line exists
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
 
-# Register your models here.
-admin.site.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ['username', 'email', 'is_staff', 'is_superuser']
+    # Add the custom 'avatar' field to the admin form
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('avatar',)}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('avatar',)}),
+    )
+
+# Unregister the default User admin if it exists, then register our custom one
+# admin.site.unregister(User) # This line is often not needed
+admin.site.register(CustomUser, CustomUserAdmin)
